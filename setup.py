a97f40cd329d03f1628147e04b5643345af58c1a
Sergii Mikhtoniuk <mikhtonyuk@gmail.com>
2013-10-20 17:28:26 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+KEYWORDS = 'networking codec connection pooling service futures finagle'.split(' ')
+SUMMARY = 'Finagle-inspired networking abstraction library for Python'
+DESCRIPTION = '''Finagle-inspired networking abstraction library for Python'''
+
+setup(name='pynnacle',
+      version='0.0.1',
+      url='https://github.com/mikhtonyuk/pynnacle',
+      author='Sergii Mikhtoniuk',
+      author_email='mikhtonyuk@gmail.com',
+      license='MIT',
+      description=SUMMARY,
+      long_description=DESCRIPTION,
+      keywords=KEYWORDS,
+      packages=find_packages(exclude=['*_test']),
+      zip_safe=False,
+      test_suite='test')
